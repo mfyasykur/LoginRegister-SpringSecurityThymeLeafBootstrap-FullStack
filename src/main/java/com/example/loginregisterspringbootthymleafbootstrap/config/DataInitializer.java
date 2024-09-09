1a09b7aa8edfd462f84763489de737d71c5450b4
mfyasykur <mf.yasykur@gmail.com>
2024-09-09 22:05:53 +0700

    Optimize code

--- src/main/java/com/example/loginregisterspringbootthymleafbootstrap/config/DataInitializer.java
+++ src/main/java/com/example/loginregisterspringbootthymleafbootstrap/config/DataInitializer.java
@@ -1,7 +1,7 @@
 package com.example.loginregisterspringbootthymleafbootstrap.config;
 
-import com.example.loginregisterspringbootthymleafbootstrap.model.Role;
-import com.example.loginregisterspringbootthymleafbootstrap.repository.RoleRepository;
+import com.example.loginregisterspringbootthymleafbootstrap.service.RoleService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
@@ -9,19 +9,18 @@ import org.springframework.stereotype.Component;
 @Component
 public class DataInitializer {
 
-    @Bean
-    public CommandLineRunner initData(RoleRepository roleRepository) {
-        return args -> {
-            createRoleIfNotFound(roleRepository, "ROLE_USER");
-            createRoleIfNotFound(roleRepository, "ROLE_ADMIN");
-        };
+    private final RoleService roleService;
+
+    @Autowired
+    public DataInitializer(RoleService roleService) {
+        this.roleService = roleService;
     }
 
-    private void createRoleIfNotFound(RoleRepository roleRepository, String roleName) {
-
-        Role role = new Role();
-        role.setName(roleName);
-
-        roleRepository.findByName(roleName).orElseGet(() -> roleRepository.save(role));
+    @Bean
+    public CommandLineRunner initData() {
+        return args -> {
+            roleService.createRoleIfNotFound("ROLE_USER");
+            roleService.createRoleIfNotFound("ROLE_ADMIN");
+        };
     }
 }
